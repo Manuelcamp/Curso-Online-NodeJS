031621f4be824411ad281bbff7805ddd0b0f8599
Manuel Almeida <ywhisgod7@gmail.com>
2026-08-21 12:30:28 -0300

    CHALLENGE__1

---
+++ Modulo3/CHALLENGE__1/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+const port =3020
+
+app.get('/', (req, res) => {
+     res.send('<h1>Bem Vindo ao meu primeiro projeto em Node.js!</h1>')
+})
+app.listen(port, () =>{
+    console.log(`Servidor rodando na porta: ${port}`)
+})
