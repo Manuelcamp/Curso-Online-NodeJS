f40285909ebbb32368b2ecf7d973d55af3dfa370
Manuel Almeida <ywhisgod7@gmail.com>
2026-08-24 10:38:19 -0300

    SETUP EXPRESS

---
+++ Modulo6/2__SETUP/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3020
+
+app.get('/', (req, res) => {
+    res.send('Olá mundo!!')
+})
+
+app.listen(port, ()=>{
+
+    console.log(`App rodando na porta: ${port}`)
+
+})
