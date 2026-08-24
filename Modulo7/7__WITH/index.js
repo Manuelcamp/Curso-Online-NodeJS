aea24fdbe37482663abf48adeee93b66bdd22617
Manuel Almeida <ywhisgod7@gmail.com>
2026-08-24 19:08:45 -0300

    WITH FUNCITON

---
+++ Modulo7/7__WITH/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashboards', (req, res) =>{
+    const items = ['item a', 'item b','item c', 'item d']
+
+    res.render('dashboards', {items})
+})
+
+app.get('/blogpost', (req, res) => {
+    const post = {
+        title: 'Aprender Java',
+        category: 'Java',
+        body: 'Este artigo fala sobre java...',
+        comments: 4,
+    } 
+
+    res.render('blogpost', {post})
+})
+
+
+
+app.get('/', (req, res) =>{
+
+    const user ={
+        name: "Matheus",
+        sobrenome: "Batista",
+        age: 30,
+    }
+
+    const palavra = 'Lamburger'
+
+    const auth = true
+
+    const approved = true
+
+    res.render('home', {user: user, palavra, auth, approved})
+
+})
+
+app.listen(5020, () =>{
+    console.log("App Funcionado")
+})
