e223ec7b8bda755e8fe8d9d382ee6e985d7f2827
Manuel Almeida <ywhisgod7@gmail.com>
2026-08-29 13:09:19 -0300

    BACK END FULL

--- Modulo8/5__FORMULARY_DATA/index.js
+++ Modulo8/5__FORMULARY_DATA/index.js
@@ -94,6 +94,25 @@ const conn = mysql.createConnection({
     database: 'nodeMySQL'
 })
 
+app.post('/books/updatebook', (req, res)=>{
+    const id = req.body.id
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+
+    const query = 
+    `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = '${id}'`
+    
+    conn.query(query, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+        
+        res.redirect('/books')
+    })
+})
+
+
 conn.connect(function(err){
     if(err){
         console.log(err)
